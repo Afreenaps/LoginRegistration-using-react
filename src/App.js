9c6b3eea3b4f735f07d384d8267321932be51290
Afreenaps <afreenaps500@gmail.com>
2025-08-11 23:21:43 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+import RegistrationForm from "./RegistrationForm";
+
+export default function App() {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLogin ? (
+        <LoginForm onSwitch={() => setIsLogin(false)} />
+      ) : (
+        <RegistrationForm onSwitch={() => setIsLogin(true)} />
+      )}
     </div>
   );
 }
 
-export default App;
